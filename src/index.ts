845fa4b3526d5425812a905a71e5f1464ffba5c6
러리 <doralife12@naver.com>
2021-07-22 01:01:09 +0900

    Feat: Default Types of Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-let message: string = 'Hello World!';
-console.log(message);
+let arr: Array<number> = [1, 2, 3];
+let x: [string, number] = ['Hello', 10];
+console.log(x);
+
+enum Color {
+  Red = 1,
+  Green = 2,
+  Blue = 4,
+}
+let color: Color = Color.Red;
+console.log(color);
+
+console.log(Color[color]);
+
+let someValue: any = 'this is a string';
+let strLength: number = (someValue as string).length;
+
+console.log(strLength);
