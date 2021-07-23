4650203863c25c7ddcc1948f96511fbe9f9f74ad
러리 <doralife12@naver.com>
2021-07-23 09:40:59 +0900

    Feat: functions in interface

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,9 @@
-interface SquareConfig {
-  color?: string;
-  width?: number;
+interface SearchFunc {
+  (source: string, subString: string): boolean;
 }
 
-interface LabeledValue {
-  label: string;
-}
-
-function createSquare(config: SquareConfig): { color: string; area: number } {
-  let newSquare = { color: 'white', area: 100 };
-  if (config.color) {
-    newSquare.color = config.color;
-  }
-  if (config.width) {
-    newSquare.area = config.width * config.width;
-  }
-  return newSquare;
-}
-
-function printLabel(labeledObj: LabeledValue) {
-  console.log(labeledObj.label);
-}
-
-const myObj2 = { colour: 100, width: 100 };
-let mySquare = createSquare(myObj2);
-let mySquare2 = createSquare({ width: 100, colour: 'Color' } as SquareConfig);
-
-const myObj = { size: 10, label: 'Size 10 Object' };
-printLabel(myObj);
+let mySearch: SearchFunc;
+mySearch = function (source, subString) {
+  let result = source.search(subString);
+  return result > -1;
+};
