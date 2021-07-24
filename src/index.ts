c7a2e5e7594c63307db32c204b08dcc0f3734c7d
러리 <doralife12@naver.com>
2021-07-24 23:10:25 +0900

    Feat: Fake this

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-interface SearchFunc {
-  (source: string, subString: string): boolean;
+interface Card {
+  suit: string;
+  card: number;
 }
 
-let mySearch: SearchFunc;
-mySearch = function (source, subString) {
-  let result = source.search(subString);
-  return result > -1;
+interface Deck {
+  suits: string[];
+  cards: number[];
+  createCardPicker(this: Deck): () => Card;
+}
+
+let deck: Deck = {
+  suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+  cards: Array(52),
+  createCardPicker: function (this: Deck) {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52);
+      let pickedSuit = Math.floor(pickedCard / 13);
+
+      return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+    };
+  },
 };
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+console.log('card: ' + pickedCard.card + ' of ' + pickedCard.suit);
