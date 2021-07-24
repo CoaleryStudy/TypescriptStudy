daceab2d7324c6f0c412add5a6574faf14816f47
러리 <doralife12@naver.com>
2021-07-24 23:26:11 +0900

    Feat: Overload

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-interface Card {
-  suit: string;
-  card: number;
+let suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+
+function pickCard(x: { suit: string; card: number }[]): number;
+function pickCard(x: number): { suit: string; card: number };
+
+function pickCard(x: any): any {
+  if (typeof x == 'object') {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  }
+  // 그렇지 않다면 그냥 card를 선택합니다.
+  else if (typeof x == 'number') {
+    let pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
 }
 
-interface Deck {
-  suits: string[];
-  cards: number[];
-  createCardPicker(this: Deck): () => Card;
-}
+let myDeck = [
+  { suit: 'diamonds', card: 2 },
+  { suit: 'spades', card: 10 },
+  { suit: 'hearts', card: 4 },
+];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+console.log('card: ' + pickedCard1.card + ' of ' + pickedCard1.suit);
 
-let deck: Deck = {
-  suits: ['hearts', 'spades', 'clubs', 'diamonds'],
-  cards: Array(52),
-  createCardPicker: function (this: Deck) {
-    return () => {
-      let pickedCard = Math.floor(Math.random() * 52);
-      let pickedSuit = Math.floor(pickedCard / 13);
-
-      return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
-    };
-  },
-};
-
-let cardPicker = deck.createCardPicker();
-let pickedCard = cardPicker();
-
-console.log('card: ' + pickedCard.card + ' of ' + pickedCard.suit);
+let pickedCard2 = pickCard(15);
+console.log('card: ' + pickedCard2.card + ' of ' + pickedCard2.suit);
