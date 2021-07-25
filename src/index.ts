8a843055d2e0d4ada7c1bc31e4b71f236bf59283
러리 <doralife12@naver.com>
2021-07-25 22:10:47 +0900

    Feat: Intersection type

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,23 @@
-type NetworkLoadingState = {
-  state: 'loading';
-};
-
-type NetworkFailedState = {
-  state: 'failed';
-  code: number;
-};
-
-type NetworkSuccessState = {
-  state: 'success';
-  response: {
-    title: string;
-    duration: number;
-    summary: string;
-  };
-};
-
-type NetworkState =
-  | NetworkLoadingState
-  | NetworkFailedState
-  | NetworkSuccessState;
-
-function networkStatus(state: NetworkState): string {
-  switch (state.state) {
-    case 'loading':
-      return 'Downloading...';
-    case 'failed':
-      return `Error ${state.code} downloading`;
-    case 'success':
-      return `Downloaded ${state.response.title} - ${state.response.summary}`;
-  }
+interface ErrorHandling {
+  success: boolean;
+  error?: { message: string };
 }
+
+interface ArtworksData {
+  artworks: { title: string }[];
+}
+
+interface ArtistsData {
+  artists: { name: string }[];
+}
+
+type ArtworksResponse = ArtworksData & ErrorHandling;
+type ArtistsResponse = ArtistsData & ErrorHandling;
+
+const handleArtistsResponse = (response: ArtistsResponse) => {
+  if (response.error) {
+    console.error(response.error.message);
+    return;
+  }
+  console.log(response.artists);
+};
