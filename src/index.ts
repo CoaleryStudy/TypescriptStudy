d08d5499e21eeb6a4dd1c3d2823d9ca6d4269372
러리 <doralife12@naver.com>
2021-07-26 00:43:49 +0900

    Feat: Class

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,30 @@
-class Person {
+abstract class Department {
   constructor(public name: string) {}
+
+  printName(): void {
+    console.log('Department name: ' + this.name);
+  }
+
+  abstract printMeeting(): void; // 반드시 파생된 클래스에서 구현되어야 합니다.
 }
 
-interface Loggable {
-  log(name: string): void;
-}
+class AccountingDepartment extends Department {
+  constructor() {
+    super('Accounting and Auditing'); // 파생된 클래스의 생성자는 반드시 super()를 호출해야 합니다.
+  }
 
-class ConsoleLogger implements Loggable {
-  log(name: string) {
-    console.log(`Hello, I'm ${name}.`);
+  printMeeting(): void {
+    console.log('The Accounting Department meets each Monday at 10am.');
+  }
+
+  generateReports(): void {
+    console.log('Generating accounting reports...');
   }
 }
 
-function extend<First extends {}, Second extends {}>(
-  first: First,
-  second: Second
-): First & Second {
-  const result: Partial<First & Second> = {};
-  for (const prop in first) {
-    if (first.hasOwnProperty(prop)) {
-      (result as First)[prop] = first[prop];
-    }
-  }
-  for (const prop in second) {
-    if (second.hasOwnProperty(prop)) {
-      (result as Second)[prop] = second[prop];
-    }
-  }
-  return result as First & Second;
-}
-
-const jim = extend(new Person('Jim'), ConsoleLogger.prototype);
-jim.log(jim.name);
+let department: Department; // 추상 타입의 레퍼런스를 생성합니다
+// department = new Department(); // 오류: 추상 클래스는 인스턴스화 할 수 없습니다
+department = new AccountingDepartment(); // 추상이 아닌 하위 클래스를 생성하고 할당합니다
+department.printName();
+department.printMeeting();
+// department.generateReports(); // 오류: 선언된 추상 타입에 메서드가 존재하지 않습니다
