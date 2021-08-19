49064deb7daaa47c36665729895bced5c5336825
러리 <doralife12@naver.com>
2021-08-19 21:33:18 +0900

    Feat: Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,54 @@
-function toHex(this: number) {
-  return this.toString(16);
+function first() {
+  console.log('first(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('first(): called');
+  };
 }
 
-function numberToString(n: ThisParameterType<typeof toHex>) {
-  return toHex.apply(n);
+function second() {
+  console.log('second(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('second(): called');
+  };
 }
 
-// ----------------------------------------------------------------------------
+class ExampleClass {
+  @first()
+  @second()
+  method() {}
+}
 
-const fiveToHex: OmitThisParameter<typeof toHex> = toHex.bind(5);
+function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
+  constructor: T
+) {
+  return class extends constructor {
+    reportingURL = 'http://www...';
+  };
+}
+
+@reportableClassDecorator
+class BugReport {
+  type = 'report';
+  title: string;
+
+  constructor(t: string) {
+    this.title = t;
+  }
+}
+
+const bug = new BugReport('Needs dark mode');
+console.log(bug.title); // Prints "Needs dark mode"
+console.log(bug.type); // Prints "report"
+
+// Note that the decorator _does not_ change the TypeScript type
+// and so the new property `reportingURL` is not known
+// to the type system:
+// bug.reportingURL;
